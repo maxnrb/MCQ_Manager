79cd1dac4ff208fce53ceb8b17dbddcde986648a
Quentin <hellfire.contact@sfr.fr>
2019-06-20 09:48:44 +0200

    Update

---
+++ server/cmake-build-debug/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/wamp64/www/MCQ_Manager/server/src/Database.cpp" "/mnt/d/wamp64/www/MCQ_Manager/server/cmake-build-debug/CMakeFiles/server.dir/src/Database.cpp.o"
+  "/mnt/d/wamp64/www/MCQ_Manager/server/src/HelloHandler.cpp" "/mnt/d/wamp64/www/MCQ_Manager/server/cmake-build-debug/CMakeFiles/server.dir/src/HelloHandler.cpp.o"
+  "/mnt/d/wamp64/www/MCQ_Manager/server/src/main.cpp" "/mnt/d/wamp64/www/MCQ_Manager/server/cmake-build-debug/CMakeFiles/server.dir/src/main.cpp.o"
+  "/mnt/d/wamp64/www/MCQ_Manager/server/src/model/Group.cpp" "/mnt/d/wamp64/www/MCQ_Manager/server/cmake-build-debug/CMakeFiles/server.dir/src/model/Group.cpp.o"
+  "/mnt/d/wamp64/www/MCQ_Manager/server/src/utils/Utils.cpp" "/mnt/d/wamp64/www/MCQ_Manager/server/cmake-build-debug/CMakeFiles/server.dir/src/utils/Utils.cpp.o"
+  "/mnt/d/wamp64/www/MCQ_Manager/server/src/utils/base64.cpp" "/mnt/d/wamp64/www/MCQ_Manager/server/cmake-build-debug/CMakeFiles/server.dir/src/utils/base64.cpp.o"
+  "/mnt/d/wamp64/www/MCQ_Manager/server/src/utils/sha1.cpp" "/mnt/d/wamp64/www/MCQ_Manager/server/cmake-build-debug/CMakeFiles/server.dir/src/utils/sha1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/soci"
+  "/usr/include/soci/mysql"
+  "/usr/include/mysql"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/cmake-build-debug/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/Database.cpp.o"
+  "CMakeFiles/server.dir/src/HelloHandler.cpp.o"
+  "CMakeFiles/server.dir/src/main.cpp.o"
+  "CMakeFiles/server.dir/src/model/Group.cpp.o"
+  "CMakeFiles/server.dir/src/utils/Utils.cpp.o"
+  "CMakeFiles/server.dir/src/utils/base64.cpp.o"
+  "CMakeFiles/server.dir/src/utils/sha1.cpp.o"
+  "server.pdb"
+  "server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
